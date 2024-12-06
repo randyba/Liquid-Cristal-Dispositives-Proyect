1517e40b92084766d188d572ea3c1741d0cfab04
randyba <randybarrantes03@gmail.com>
2024-12-06 13:05:42 -0600

    actulizacion12/06

--- App.js
+++ App.js
@@ -11,24 +11,53 @@ function myFunction() {
   }
 }
 
+const locales = ["en-GB","de-DE","es-ES","fr-FR","it-IT","pt-BR"];
 
-function myFunction() {   
-  //Añade una clase al elemento que tenga el id myDropdown
-  document.getElementById("myDropdown").classList.toggle("show");
+function getFlagSrc(countryCode) {
+  return /^[A-Z]{2}$/.test(countryCode)
+       ? `https://flagsapi.com/${countryCode.toUpperCase()}/shiny/64.png`
+    : "";
 }
 
-//Cierra el submenu si se clica fuera
-window.onclick = function(event){
-  if(!event.target.matches('.drop-button')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0;  i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      //Busca dentro de drop-content los elementos con la clase show
-      if (openDropdown.classList.contains('show')){
-        //elimina la clase show de los elementos dentro de drop-content
-        openDropdown.classList.remove('show');
-      }
-    }
+const dropdownBtn = document.getElementById("dropdown-btn");
+const dropdownContent = document.getElementById("dropdown-content");
+
+function setSelectedLocale(locale) {
+  const intlLocale = new Intl.Locale(locale);
+  const langName = new Intl.DisplayNames([locale], {
+    type: "language",
+  }).of(intlLocale.language);
+
+  dropdownContent.innerHTML = "";
+
+  const otherLocales = locales.filter((loc) => loc !== locale);
+  otherLocales.forEach((otherLocale) => {
+    const otherIntlLocale = new Intl.Locale(otherLocale);
+    const otherLangName = new Intl.DisplayNames([otherLocale], {
+      type: "language",
+    }).of(otherIntlLocale.language);
+
+    const listEl = document.createElement("li");
+    listEl.innerHTML = `${otherLangName}<img src="${getFlagSrc(
+      otherIntlLocale.region
+    )}" />`;
+    listEl.value = otherLocale;
+    listEl.addEventListener("mousedown", function () {
+      setSelectedLocale(otherLocale);
+    });
+    dropdownContent.appendChild(listEl);
+  });
+
+  dropdownBtn.innerHTML = `<img src="${getFlagSrc(
+    intlLocale.region
+  )}" />${langName}<span class="arrow-down"></span>`;
+}
+
+setSelectedLocale(locales[0]);
+const browserLang = new Intl.Locale(navigator.language).language;
+for (const locale of locales) {
+  const localeLang = new Intl.Locale(locale).language;
+  if (localeLang === browserLang) {
+    setSelectedLocale(locale);
   }
 }
